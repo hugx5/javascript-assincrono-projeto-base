7fac173e22cfbcafc46b88298b81b94b96af5896
vitor hugo <rodriguesx5ti@gmail.com>
2025-01-02 09:28:02 -0300

    correção

--- scripts.js
+++ scripts.js
@@ -1,31 +1,38 @@
-const uploadBtn = document.getElementById("upload-btn"); // Get the upload button
-const inputUpload = document.getElementById("image-upload") // Get the input upload
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload")
 
 uploadBtn.addEventListener("click", () => {
-    inputUpload.click(); //
+    inputUpload.click();
 })
 
-function lerConteudoDoArquivo(arquivo) { // Função para ler o conteúdo do arquivo
-    return new Promise((resolve, reject) => { // Retorna uma promessa       
-        const leitor = new FileReader(); // Cria um novo objeto FileReader
-        leitor.onload = () => { // Quando o arquivo é carregado
-            resolve({URL: leitor.result, nome: arquivo.name}) // Resolve a promessa com o resultado da leitura
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name })
         }
+
         leitor.onerror = () => {
-            reject('Erro na leitura do arquivo ${arquivo.name}') // Rejeita a promessa com o erro
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
         }
-        leitor.readAsDataURL(arquivo); // Lê o arquivo como uma URL
-        
-})
 
-inputUpload.addEventListener("change", async (eventos) => {
-    const arquivo = eventos.target.files[0];
-    
-    if(arquivo) {
-        try{
+        leitor.readAsDataURL(arquivo)
+    })
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
             const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
-        
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo")
         }
     }
 })
-    
